f341aa75d1d9f986a7be16ccbace1a5599be021e
Enes Kızılcin <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-11-27 22:32:49 +0300

    project initalized from vs2019

---
+++ SportFixtureTracking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportFixtureTracking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
