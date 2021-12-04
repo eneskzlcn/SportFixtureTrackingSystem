9384f1183d106c46fdceddd6b037d845b3d2e58a
Enes Kızılcin <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-12-04 21:10:27 +0300

    teams controller with mvc entity with views added

---
+++ SportFixtureTracking/Controllers/TeamsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportFixtureTracking.Models;
+
+namespace SportFixtureTracking.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly SportFixturePointContext _context;
+
+        public TeamsController(SportFixturePointContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index()
+        {
+            var sportFixturePointContext = _context.Teams.Include(t => t.Club).Include(t => t.Sport);
+            return View(await sportFixturePointContext.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.Club)
+                .Include(t => t.Sport)
+                .FirstOrDefaultAsync(m => m.TeamId == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public IActionResult Create()
+        {
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "ClubName");
+            ViewData["SportId"] = new SelectList(_context.Sports, "SportId", "SportName");
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeamId,ClubId,SportId,TeamName,PlayerCount")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "ClubName", team.ClubId);
+            ViewData["SportId"] = new SelectList(_context.Sports, "SportId", "SportName", team.SportId);
+            return View(team);
+        }
+
+        // GET: Teams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "ClubName", team.ClubId);
+            ViewData["SportId"] = new SelectList(_context.Sports, "SportId", "SportName", team.SportId);
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TeamId,ClubId,SportId,TeamName,PlayerCount")] Team team)
+        {
+            if (id != team.TeamId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(team);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamExists(team.TeamId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClubId"] = new SelectList(_context.Clubs, "ClubId", "ClubName", team.ClubId);
+            ViewData["SportId"] = new SelectList(_context.Sports, "SportId", "SportName", team.SportId);
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.Club)
+                .Include(t => t.Sport)
+                .FirstOrDefaultAsync(m => m.TeamId == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int id)
+        {
+            return _context.Teams.Any(e => e.TeamId == id);
+        }
+    }
+}
