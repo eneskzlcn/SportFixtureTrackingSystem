b87db643a79095eaf36483e2cc796b8317ba7bd7
Enes Kızılcin <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-12-04 22:37:24 +0300

    icon added for homepage, layout stlye changed, fontawesome lib added,

---
+++ SportFixtureTracking/Controllers/FixturesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportFixtureTracking.Models;
+
+namespace SportFixtureTracking.Controllers
+{
+    public class FixturesController : Controller
+    {
+        private readonly SportFixturePointContext _context;
+
+        public FixturesController(SportFixturePointContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Fixtures
+        public async Task<IActionResult> Index()
+        {
+            var sportFixturePointContext = _context.Fixtures.Include(f => f.AwayTeam).Include(f => f.HomeTeam);
+            return View(await sportFixturePointContext.ToListAsync());
+        }
+
+        // GET: Fixtures/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fixture = await _context.Fixtures
+                .Include(f => f.AwayTeam)
+                .Include(f => f.HomeTeam)
+                .FirstOrDefaultAsync(m => m.FixtureId == id);
+            if (fixture == null)
+            {
+                return NotFound();
+            }
+
+            return View(fixture);
+        }
+
+        // GET: Fixtures/Create
+        public IActionResult Create()
+        {
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName");
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName");
+            return View();
+        }
+
+        // POST: Fixtures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FixtureId,FixtureDate,HomeTeamId,AwayTeamId,FixtureHome,IsFinished")] Fixture fixture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(fixture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName", fixture.AwayTeamId);
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName", fixture.HomeTeamId);
+            return View(fixture);
+        }
+
+        // GET: Fixtures/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fixture = await _context.Fixtures.FindAsync(id);
+            if (fixture == null)
+            {
+                return NotFound();
+            }
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName", fixture.AwayTeamId);
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName", fixture.HomeTeamId);
+            return View(fixture);
+        }
+
+        // POST: Fixtures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FixtureId,FixtureDate,HomeTeamId,AwayTeamId,FixtureHome,IsFinished")] Fixture fixture)
+        {
+            if (id != fixture.FixtureId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fixture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FixtureExists(fixture.FixtureId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName", fixture.AwayTeamId);
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName", fixture.HomeTeamId);
+            return View(fixture);
+        }
+
+        // GET: Fixtures/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fixture = await _context.Fixtures
+                .Include(f => f.AwayTeam)
+                .Include(f => f.HomeTeam)
+                .FirstOrDefaultAsync(m => m.FixtureId == id);
+            if (fixture == null)
+            {
+                return NotFound();
+            }
+
+            return View(fixture);
+        }
+
+        // POST: Fixtures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fixture = await _context.Fixtures.FindAsync(id);
+            _context.Fixtures.Remove(fixture);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FixtureExists(int id)
+        {
+            return _context.Fixtures.Any(e => e.FixtureId == id);
+        }
+    }
+}
